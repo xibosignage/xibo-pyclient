b39bf8454e274001f96012d81d8647313bdeb660
Alex Harrington <alex@longhill.org.uk>
2009-04-01 21:19:17 +0100

    [pyclient] Refactored as per suggestions in Dive into Python

--- python/XiboClient.py
+++ python/XiboClient.py
@@ -13,3 +13,37 @@ class XiboClient:
 xc = XiboClient()
 xc.play(xc)
 
+class XiboDisplayManager:
+    def __init__(self):
+        pass
+    
+class XiboDownloadManager:
+    def __init__(self):
+        pass
+
+class XiboDownloadThread:
+    def __init__(self):
+        pass
+
+class XiboLayout:
+    def __init__(self):
+        pass
+
+class Xmds:
+    def __init__(self):
+        pass
+
+class XiboLayoutManager:
+    def __init__(self):
+        pass
+
+class XiboRegionManager:
+    def __init__(self):
+        pass
+
+class XiboMediaInterface:
+    pass
+
+class XiboMedia:
+    def __init(self):
+        pass
