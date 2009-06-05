415ad0a4c3d9e6fba43db672d291f53d1b7b36ea
Alex Harrington <alex@longhill.org.uk>
2009-06-05 23:48:39 +0100

    [pyclient] Added support for capture from v4l2 devices

---
+++ python/plugins/media/TvMedia.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from XiboMedia import XiboMedia
+from threading import Thread
+
+class TvMedia(XiboMedia):
+    def run(self):
+	tmpXML = '<camera device="' + str(self.options['device']) + '" source="' + str(self.options['driver']) + '" id="' + self.mediaNodeName + '" capturewidth="640" captureheight="480" pixelformat="YUYV422" framerate="30" />'
+	self.p.enqueue('add',(tmpXML,self.regionNodeName))
+	self.p.enqueue('play', self.mediaNodeName)
+	self.p.enqueue('resize',(self.mediaNodeName, self.width, self.height))
+	self.p.enqueue('timer',(int(self.duration) * 1000,self.parent.next))
