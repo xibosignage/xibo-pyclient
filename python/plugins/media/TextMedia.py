9ef257b40c0dbb936fa07cf65dc77376fa3f8767
Alex Harrington <alex@longhill.org.uk>
2009-10-25 14:02:52 +0000

    [pyclient] Fixed a couple of silly issues in the previous commit. Now working.

--- python/plugins/media/TextMedia.py
+++ python/plugins/media/TextMedia.py
@@ -43,9 +43,10 @@ class TextMedia(XiboMedia):
         for t in text:
             self.textNode = t
         
-        for node in self.textNode:
-            node.nodeType == node.TEXT_NODE:
+        for node in self.textNode.childNodes:
+            if node.nodeType == node.TEXT_NODE:
                 html = node.data
+                log.log(7,'HTML to display is: ' + html)
         
         html = """<p><span style="color: rgb(255, 255, 255);"><span style="font-size: 1.6em;"><span style="font-family: Arial;">There should be some flowers above me!</span></span></span></p>"""
         parser = HTMLPango()
