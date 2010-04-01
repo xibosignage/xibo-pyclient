830c03bf7b8061074b1772bb763aa51f4ccdca9d
Alex Harrington <alex@longhill.org.uk>
2010-04-01 15:00:53 +0100

    [pyclient] Added MicroblogSearchMedia media plugin stub

---
+++ python/plugins/media/MicroblogSearchMedia.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# Xibo - Digitial Signage - http://www.xibo.org.uk
+# Copyright (C) 2009 Alex Harrington
+#
+# This file is part of Xibo.
+#
+# Xibo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version. 
+#
+# Xibo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from XiboMedia import XiboMedia
+from threading import Thread
+import urllib
+import sys, os, time, codecs
+import twython
+
+class MicroblogSearchMedia(XiboMedia):
+    def add(self):
+        
+    def run(self):
+        
+    def dispose(self):
+        pass
